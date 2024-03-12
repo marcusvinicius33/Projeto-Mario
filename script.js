6c1532af1588a816a478ba204f4d02052a8c5912
seu-username <vinicius.vigia@gmail.com>
2024-03-11 22:42:14 -0300

    projeto adicionado

---
+++ script.js
@@ -0,0 +1,14 @@
+const form = document.querySelector(".formulario-fale-conosco")
+const mascara = document.querySelector(".mascara-formulario")
+
+function cliqueiNoBotao(){
+    form.style.left = "50%"
+    form.style.transform = "translatex(-50%)"
+    mascara.style.visibility = "visible"
+}
+
+function cliqueiNaMascara(){
+    mascara.style.visibility = "hidden"
+    form.style.left = "-300px"
+
+}
